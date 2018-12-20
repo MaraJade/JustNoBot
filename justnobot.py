83e9e747b2ee7c91a7eedecb9515a3e2b8736b5d
Mara Jade <catbookgirl@gmail.com>
2018-12-19 21:45:47 -0800

    attempting splitlines to fix ascii problems

--- justnobot.py
+++ justnobot.py
@@ -128,7 +128,7 @@ def is_marked(post):
 def get_messages():
     for message in reddit.inbox.unread(limit=100):
         print(message)
-        parts = message.body.split(' ')
+        parts = message.body.split(' ').splitlines()
         if message.subject == "Subscribe" and len(parts) > 2:
             addSubscriber(message.author, parts[1], parts[2])
         elif message.subject == "Unsubscribe" and len(parts) > 2:
