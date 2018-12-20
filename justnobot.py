c2ada084df06c64450301c0225e0b35169fce776
Mara Jade <catbookgirl@gmail.com>
2018-12-19 20:08:43 -0800

    I think I broke the post thing

--- justnobot.py
+++ justnobot.py
@@ -136,6 +136,7 @@ def get_messages():
 
 def get_posts(subreddit):
     for post in subreddit.new(limit=100):
+        print(is_marked(post))
         if duplicate_preventer(post):
             continue
         elif post.author is not None and is_marked(post) is None:
