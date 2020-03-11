1185cc21c93b097bd0a34aca8ec4151d94ac9c89
Mara Jade <catbookgirl@gmail.com>
2020-03-11 12:49:48 -0700

    Forgot to make sure configs weren't being pushed

--- config.py
+++
@@ -1,9 +0,0 @@
-client_id = 'xWYglsZXi8-3dw' # enter user ID
-client_secret = 'mz-WTBBSG-5Xj3ZNG40s9MIKkpc' # enter user secret ID
-username = 'TheJustNoBot' # enter user username
-password = '#garbagemods2019' # enter user password
-user_agent = 'JustNoBot/0.1 by Mara' # user agent
-
-bot_name = "TheJustNoBot" # bot name
-db_name = "justno.db" # database name
-
