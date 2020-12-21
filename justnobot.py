19a71d0c843e4539684aed9fd73aefe988479963
EC2 Default User <ec2-user@ip-172-31-14-55.us-west-1.compute.internal>
2020-12-21 00:45:59 +0000

    Pushing changes made when live
    You know, when I write it out, that sounds like a really bad idea.
    I should stop doing that.

--- justnobot.py
+++ justnobot.py
@@ -115,6 +115,7 @@ class bot():
 
         # Saving time by checking if we've already looked at the post
         def is_marked(self, post):
+                #print("Checking if post is marked")
                 self.db_connection.cursor().execute('''SELECT count (*) FROM marked_posts WHERE PostID = ?''', (str(post),))
 
                 return self.db_connection.cursor().fetchone()
@@ -122,6 +123,7 @@ class bot():
 
         # Checking if the post has already been responded to
         def sticky_checker(self, post):
+            #print("Checking for sticky")
             bot = False
             stickied = False
             for comment in list(post.comments):
@@ -160,6 +162,7 @@ class bot():
                                 body = "You have successfully been subscribed to {} in {}! I will notify you whenever they post.".format(str(parts[1]), str(parts[2]))
 
                                 self.reddit.redditor(str(message.author)).message(subject=subject, message=body) 
+                                print("Subscriber notified")
                         elif message.subject == "Unsubscribe" and len(parts) > 2:
                                 self.removeSubscriber(message.author, parts[1], parts[2])
 
@@ -171,6 +174,16 @@ class bot():
 
                         message.mark_read()
 
+        # Make sure the user exists
+        def user_exists(self, name):
+                #print("Checking if user exists")
+                try:
+                        user = self.reddit.redditor(str(name)).id
+                        #print("User exists:", user)
+                except Exception as e:
+                        print(e)
+                        return False
+                return True
 
         # Go though all the posts on the sub
         def get_posts(self):
@@ -182,11 +195,17 @@ class bot():
 
                         # Check for stickies
                         sticky = self.sticky_checker(post)
+                        #print("Sticky:", sticky)
+
+                        #print(post.author)
+                        #author_exists = self.user_exists(post.author)
+                        #print(author_exists)
 
                         # Make sure the author has not deleted their account,
                         # the post isn't locked, and we haven't already posted
                         # on it
-                        if post.author is not None and self.is_marked(post) is None and sticky[0] == False:
+                        if (self.user_exists(post.author) == True) and (self.is_marked(post) == None) and (sticky[0] == False):
+                                print("Commenting")
                                 subreddit = post.subreddit
                                 all_rules = "**Quick Rule Reminders:**\n\nOP's needs come first, avoid dramamongering, respect the flair, and don't be an asshole. If your only advice is to jump straight to NC or divorce, your comment may be subject to removal at moderator discretion.\n\n[**^(Full Rules)**](https://www.reddit.com/r/{}/wiki/index#wiki_rules) ^(|) [^(Acronym Index)](https://www.reddit.com/r/{}/wiki/index#wiki_acronym_dictionary) ^(|) [^(Flair Guide)](https://www.reddit.com/r/{}/wiki/index#wiki_post_flairs)^(|) [^(Report PM Trolls)](https://www.reddit.com/r/{}/wiki/index#wiki_trolls_suck)\n\n**Resources:** [^(In Crisis?)](https://www.reddit.com/r/JustNoNetwork/wiki/links#wiki_crisis_links.3A_because_there.2019s_more_than_one_type_of_crisis) ^(|) [^(Tips for Protecting Yourself)](https://www.reddit.com/r/JUSTNOMIL/wiki/index#wiki_protecting_yourself) ^(|) [^(Our Book List)](https://www.reddit.com/r/JustNoNetwork/wiki/books) ^(|) [^(This Sub's Wiki)](https://www.reddit.com/r/{}/wiki/) ^(|) [^(General Resources)](https://www.reddit.com/r/JustNoNetwork/wiki/tos)\n\n".format(subreddit, subreddit, subreddit, subreddit, subreddit)
 
